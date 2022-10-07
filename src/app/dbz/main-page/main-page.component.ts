883e979ad97aa4125aa20ba510d75b23a85fc9f1
jcarloshg <carlosj12336@gmail.com>
2022-10-07 18:57:09 -0500

    62 input

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+
+interface Personaje {
+  nombre: string,
+  poder: number,
+}
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 14000
+    }
+  ]
+
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  cambiarNombre(event: any) {
+    console.log(event.target.value);
+
+  }
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+
+  }
+}
